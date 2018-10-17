8f251cdb9b53f607dde03bb0ddfb9b2914a91c47
PortatilJuan <juan.alvarez@escuelaestech.es>
2018-10-18 01:28:15 +0200

    Ejercicio 1 terminado.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("En la ecuación x2 - A = 0");
+        System.out.println("Introduzca un valor para A (Número entero)");
+        int a = sc.nextInt();
+        double x = (double) Math.sqrt(a);
+
+        if (a<0) {
+            System.out.println("La ecuación no tiene solución");
+        }
+        else if (a==0) {
+            System.out.println("El resultado de la ecuación es x= 0");
+        } else {
+            System.out.println("El resultado de la ecuación es x= "+x);
+            System.out.println("Y la segunda solución es x = "+-x);
+        }
+
     }
 }
