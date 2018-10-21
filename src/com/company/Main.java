d3751df36acbb1fae941700f68ad84d5e144d6c3
Notebook-Juan <juan.alvarez@escuelaestech.es>
2018-10-21 12:39:13 +0200

    Ejercicio 2 terminado.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,29 @@
 package com.company;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("En la ecuación x2 - A = 0");
-        System.out.println("Introduzca un valor para A (Número entero)");
-        int a = sc.nextInt();
-        double x = (double) Math.sqrt(a);
 
-        if (a<0) {
-            System.out.println("La ecuación no tiene solución");
-        }
-        else if (a==0) {
-            System.out.println("El resultado de la ecuación es x= 0");
-        } else {
-            System.out.println("El resultado de la ecuación es x= "+x);
-            System.out.println("Y la segunda solución es x = "+-x);
-        }
+        // Abrimos la variable escaner para la introducción de datos mediante teclado.
+        Scanner sc = new Scanner(System.in);
+
+        //Información para el usuario
+        System.out.println("En la siguiente ecuación calcularemos el área de un circulo.");
+        System.out.println("Introduzca un valor del radio del circulo");
+
+        //iniciamos la variable r que corresponde al radio
+        double r = sc.nextDouble();
+
+        //Hacemos los calculos de la formula y seguidamente aproximamos los decimales a 3 digitos
+        double resultado = Math.PI*(Math.pow(r,2));
+        DecimalFormat aprox = new DecimalFormat("#.000");
+
+        //Mostramos el resultado al usuario
+        System.out.println("El área del circulo indicado es: " + aprox.format(resultado));
 
     }
 }
