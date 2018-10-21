5c041e488fb41593e5d44ddd0f49fd0a5379e7ee
Notebook-Juan <juan.alvarez@escuelaestech.es>
2018-10-21 22:26:21 +0200

    Ejercicio 7 terminado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,22 +12,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         //Información para el usuario
-        System.out.println("En este ejercicio el usuario introducirá dos números enteros y el programa dirá si son multipolos o no");
+        System.out.println("En este ejercicio el usuario introducirá dos números y el programa dira cual es mayor o si son iguales");
 
         //Pedimos al usuario que introduzca el primer número
         System.out.println("Introduzca el primer número entero");
         //Guardamos en la variable el primer valor
-        int num1 = sc.nextInt();
+        double num1 = sc.nextDouble();
         //Pedimos al usuario que introduzca el segundo número
         System.out.println("Introduzca el segundo número entero");
         // Guardamos en la variable num2 el valor introducido
-        int num2 = sc.nextInt();
+        double num2 = sc.nextDouble();
 
         //Se hacen las comparaciones para indicar si es multiplo o no
-        if (num1%num2==0) {
-            System.out.printf("El número %d es multiplo de %d",num1,num2);
-        } else
-            System.out.printf("El número %d no es multiplo de %d",num1,num2);
+        if (num1>num2) {
+            System.out.println("El numero mayor es: "+num1);
+        } else if (num2>num1) {
+            System.out.println("El número mayor es: "+num2);
+        } else if (num1==num2) {
+            System.out.println("Los numeros son iguales");
         }
+    }
 
 }
