2764de3a594aacd6610be593b0aa7085e0606421
Notebook-Juan <juan.alvarez@escuelaestech.es>
2018-10-21 14:24:03 +0200

    Ejercicio 3 terminado.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,18 +12,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         //Información para el usuario
-        System.out.println("En la siguiente ecuación calcularemos el área de un circulo.");
+        System.out.println("En la siguiente ecuación calcularemos la longitud de un circulo.");
         System.out.println("Introduzca un valor del radio del circulo");
 
         //iniciamos la variable r que corresponde al radio
         double r = sc.nextDouble();
 
         //Hacemos los calculos de la formula y seguidamente aproximamos los decimales a 3 digitos
-        double resultado = Math.PI*(Math.pow(r,2));
+        double resultado = 2*Math.PI*r;
         DecimalFormat aprox = new DecimalFormat("#.000");
 
         //Mostramos el resultado al usuario
-        System.out.println("El área del circulo indicado es: " + aprox.format(resultado));
+        System.out.println("La longitud del circulo indicado es: " + aprox.format(resultado));
 
     }
 }
