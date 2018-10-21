8e3fbfd9b336b971b9c67dba549e5d3f22ef76d9
Notebook-Juan <juan.alvarez@escuelaestech.es>
2018-10-21 23:55:12 +0200

    Ejercicio 9 terminado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,19 +18,30 @@ public class Main {
         System.out.println("Introduzca el primer número");
         //Guardamos en la variable el primer valor
         double num1 = sc.nextDouble();
-        //Pedimos al usuario que introduzca el segundo número
         System.out.println("Introduzca el segundo número");
-        // Guardamos en la variable num2 el valor introducido
         double num2 = sc.nextDouble();
+        System.out.println("Introduzca el tercer número");
+        double num3 = sc.nextDouble();
+
 
         //Se hacen las comparaciones para ordenarlos
-        if (num1>num2) {
-            System.out.println(num1 +" " + num2);
-        } else if (num2>num1) {
-            System.out.println(num2 + " "+ num1);
-        } else if (num1==num2) {
-            System.out.println("Los numeros son iguales");
+
+        if (num1>num2&&num2>num3) {
+            System.out.println(num1+" "+num2+" "+num3);
+        } else if (num1>num3&&num3>num2) {
+            System.out.println(num1+" "+num3+" "+num2);
+        } else if (num2>num1&&num1>num3) {
+            System.out.println(num2+" "+num1+" "+num3);
+        } else if (num2>num3&&num2>num1) {
+            System.out.println(num2+" "+num3+" "+num1);
+        } else if (num3>num1&&num1>num2) {
+            System.out.println(num3+" "+num1+" "+num2);
+        } else if (num3>num2&&num2>num1) {
+            System.out.println(num3+" "+num2+" "+num1);
+        } else {
+            System.out.println("Hay números que se repiten  y no se pueden ordenar");
         }
+
     }
 
 }
