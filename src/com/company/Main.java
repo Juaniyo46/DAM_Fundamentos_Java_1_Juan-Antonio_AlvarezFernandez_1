ccad24afebe1d36ae84d133b565adff5791a247b
Notebook-Juan <juan.alvarez@escuelaestech.es>
2018-10-21 14:51:38 +0200

    Ejercicio 5 terminado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,18 +12,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         //Información para el usuario
-        System.out.println("En este ejercicio el usuario introducirá un numero (positivo o negativo) y el programa dira si es positivo o negativo");
-        System.out.println("Introduzca un número");
+        System.out.println("En este ejercicio el usuario introducirá dos número y el programa dirá si son iguales o no");
 
-        //iniciamos la variable r que corresponde al radio
-        double num = sc.nextDouble();
+        //Pedimos al usuario que introduzca el primer número
+        System.out.println("Introduzca el primer número");
+        //Guardamos en la variable el primer valor
+        double num1 = sc.nextDouble();
+        //Pedimos al usuario que introduzca el segundo número
+        System.out.println("Introduzca el segundo número");
+        // Guardamos en la variable num2 el valor introducido
+        double num2 = sc.nextDouble();
 
-        if (num>=0) {
-            System.out.println("El número introducido es positivo");
-        } else if (num<0) {
-            System.out.println("El número introducido es negativo");
+        //Se hacen las comparaciones para indicar si es igual o no
+        if (num1==num2) {
+            System.out.println("Los números introducidos son iguales");
+        } else
+            System.out.println("Los números introducidos no son iguales");
         }
 
-
-    }
 }
