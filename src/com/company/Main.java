5779d3868eb263cd00c403ff689e7cdad1f254c7
Notebook-Juan <juan.alvarez@escuelaestech.es>
2018-10-21 22:55:01 +0200

    Ejercicio 8 terminado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,22 +12,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         //Información para el usuario
-        System.out.println("En este ejercicio el usuario introducirá dos números y el programa dira cual es mayor o si son iguales");
+        System.out.println("En este ejercicio el usuario introducirá dos números y el programa lor ordenará de mayor a menor");
 
         //Pedimos al usuario que introduzca el primer número
-        System.out.println("Introduzca el primer número entero");
+        System.out.println("Introduzca el primer número");
         //Guardamos en la variable el primer valor
         double num1 = sc.nextDouble();
         //Pedimos al usuario que introduzca el segundo número
-        System.out.println("Introduzca el segundo número entero");
+        System.out.println("Introduzca el segundo número");
         // Guardamos en la variable num2 el valor introducido
         double num2 = sc.nextDouble();
 
-        //Se hacen las comparaciones para indicar si es multiplo o no
+        //Se hacen las comparaciones para ordenarlos
         if (num1>num2) {
-            System.out.println("El numero mayor es: "+num1);
+            System.out.println(num1 +" " + num2);
         } else if (num2>num1) {
-            System.out.println("El número mayor es: "+num2);
+            System.out.println(num2 + " "+ num1);
         } else if (num1==num2) {
             System.out.println("Los numeros son iguales");
         }
