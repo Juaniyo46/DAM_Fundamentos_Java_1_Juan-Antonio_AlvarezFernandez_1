e921a1353703647af4bbb5d0f899717fe4bd1246
Portatil-Juan <juan.alvarez@escuelaestech.es>
2018-11-04 12:47:29 +0100

    Ejericio 22 terminado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,126 +12,109 @@ public class Main<Ejercicio5> {
         Scanner sc1 = new Scanner(System.in);
 
         //Información para el usuario
-        System.out.println("En este ejercicio se pediran dos numeros y el usuario elegira que ejercicios de los anteriores 5, 6, 7 o 8 hacer.");
+        System.out.println("En este ejercicio se pediran dos numeros y el usuario elegira que ejercicios de los anteriores 10, 11 o 12 hacer.");
 
         //Pedimos al usuario que introduzca el número del menú
-        System.out.println("Introduzca el número de ejercicio a realizar (5,6,7 o 8)");
+        System.out.println("Introduzca el número de ejercicio a realizar (10, 11 o 12)");
         //Guardamos en la variable el primer valor
         int numEjercicio = sc1.nextInt();
 
-        if (numEjercicio==5) {
-            Ejercicio5();
+        if (numEjercicio==10) {
+            Ejercicio10();
 
-        } else if (numEjercicio==6) {
-            Ejercicio6();
-        }else if (numEjercicio==7) {
-            Ejercicio7();
-        }else if (numEjercicio==8) {
-            Ejercicio8();
-        }
+        } else if (numEjercicio==11) {
+            Ejercicio11();
+        }else if (numEjercicio==12) {
+            Ejercicio12();
+        }else System.out.println("El número introducido no corresponde con ningún ejercicio");
 
 
 
 
     }
 
-    public static void Ejercicio5 () {
+    public static void Ejercicio10 () {
         // Abrimos la variable escaner para la introducción de datos mediante teclado.
         Scanner sc = new Scanner(System.in);
 
         //Información para el usuario
-        System.out.println("En este ejercicio el usuario introducirá dos número y el programa dirá si son iguales o no");
+        System.out.println("En este ejercicio el usuario introducirá un numero entre 0 y 9999 y mostrara el número de cifras");
 
         //Pedimos al usuario que introduzca el primer número
-        System.out.println("Introduzca el primer número");
-        //Guardamos en la variable el primer valor
-        double num1 = sc.nextDouble();
-        //Pedimos al usuario que introduzca el segundo número
-        System.out.println("Introduzca el segundo número");
-        // Guardamos en la variable num2 el valor introducido
-        double num2 = sc.nextDouble();
-
-        //Se hacen las comparaciones para indicar si es igual o no
-        if (num1==num2) {
-            System.out.println("Los números introducidos son iguales");
-        } else
-            System.out.println("Los números introducidos no son iguales");
-
-    }
-
-    public static void Ejercicio6 () {
-        // Abrimos la variable escaner para la introducción de datos mediante teclado.
-        Scanner sc = new Scanner(System.in);
-
-        //Información para el usuario
-        System.out.println("En este ejercicio el usuario introducirá dos números enteros y el programa dirá si son multipolos o no");
-
-        //Pedimos al usuario que introduzca el primer número
-        System.out.println("Introduzca el primer número entero");
+        System.out.println("Introduzca un número dentro del rango");
         //Guardamos en la variable el primer valor
         int num1 = sc.nextInt();
-        //Pedimos al usuario que introduzca el segundo número
-        System.out.println("Introduzca el segundo número entero");
-        // Guardamos en la variable num2 el valor introducido
-        int num2 = sc.nextInt();
 
-        //Se hacen las comparaciones para indicar si es multiplo o no
-        if (num1%num2==0) {
-            System.out.printf("El número %d es multiplo de %d",num1,num2);
-        } else
-            System.out.printf("El número %d no es multiplo de %d",num1,num2);
+        //Se hacen las comparaciones y operaciones para mostrar el numero de cifras.
+        if (num1>=0&&num1<=9999) {
+            String num = String.valueOf(num1);
+            System.out.println("El numero introducido tiene "+num.length()+" cifras");
 
+        } else {
+            System.out.println("El número introducido esta fura del rango 0 y 9999");
+        }
     }
 
-    public static void Ejercicio7 () {
+    public static void Ejercicio11 () {
         // Abrimos la variable escaner para la introducción de datos mediante teclado.
         Scanner sc = new Scanner(System.in);
 
         //Información para el usuario
-        System.out.println("En este ejercicio el usuario introducirá dos números y el programa dira cual es mayor o si son iguales");
+        System.out.println("En este ejercicio el usuario introducirá un numero entre 0 y 9999 y mostrara las cifras al revés");
 
         //Pedimos al usuario que introduzca el primer número
-        System.out.println("Introduzca el primer número entero");
+        System.out.println("Introduzca un número dentro del rango");
         //Guardamos en la variable el primer valor
-        double num1 = sc.nextDouble();
-        //Pedimos al usuario que introduzca el segundo número
-        System.out.println("Introduzca el segundo número entero");
-        // Guardamos en la variable num2 el valor introducido
-        double num2 = sc.nextDouble();
+        int num1 = sc.nextInt();
 
-        //Se hacen las comparaciones para indicar si es multiplo o no
-        if (num1>num2) {
-            System.out.println("El numero mayor es: "+num1);
-        } else if (num2>num1) {
-            System.out.println("El número mayor es: "+num2);
-        } else if (num1==num2) {
-            System.out.println("Los numeros son iguales");
+        //Se hacen las comparaciones y operaciones para mostrar el numero de cifras.
+        if (num1>=0&&num1<=9999) {
+            String num = String.valueOf(num1);
+            String reves ="";
+
+            char[] numReves = num.toCharArray();
+
+            for (int i=numReves.length-1; i>-1;i--) {
+                reves+=numReves[i]+"";
+            }
+            System.out.println("El numero introducido con las cifras al reves es: "+reves);
+
+        } else {
+            System.out.println("El número introducido esta fura del rango 0 y 9999");
         }
     }
-    public static void Ejercicio8 () {
+
+    public static void Ejercicio12 () {
         // Abrimos la variable escaner para la introducción de datos mediante teclado.
         Scanner sc = new Scanner(System.in);
 
         //Información para el usuario
-        System.out.println("En este ejercicio el usuario introducirá dos números y el programa lor ordenará de mayor a menor");
+        System.out.println("En este ejercicio el usuario introducirá un numero y le programa dira si es capicua o no");
 
         //Pedimos al usuario que introduzca el primer número
-        System.out.println("Introduzca el primer número");
+        System.out.println("Introduzca un número dentro del rango 0 y 9999");
         //Guardamos en la variable el primer valor
-        double num1 = sc.nextDouble();
-        //Pedimos al usuario que introduzca el segundo número
-        System.out.println("Introduzca el segundo número");
-        // Guardamos en la variable num2 el valor introducido
-        double num2 = sc.nextDouble();
+        int num1 = sc.nextInt();
 
-        //Se hacen las comparaciones para ordenarlos
-        if (num1>num2) {
-            System.out.println(num1 +" " + num2);
-        } else if (num2>num1) {
-            System.out.println(num2 + " "+ num1);
-        } else if (num1==num2) {
-            System.out.println("Los numeros son iguales");
+        //Se hacen las comparaciones para averiguar el número al reves y despues compararlo con el numero normal
+        if (num1>=0&&num1<=9999) {
+            String num = String.valueOf(num1);
+            String reves ="";
+
+            char[] numReves = num.toCharArray();
+
+            for (int i=numReves.length-1; i>-1;i--) {
+                reves+=numReves[i];
+            }
+            if (reves.equals(num)) {
+                System.out.println("El número es capicua");
+            } else System.out.println("El número no es capicua");
+
+
+        } else {
+            System.out.println("El número introducido esta fuera del rango 0 y 9999");
         }
+
     }
 
 }
