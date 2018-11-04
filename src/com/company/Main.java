29e6efb8c65ad1f033f916668eb84f78193fcf21
Portatil-Juan <juan.alvarez@escuelaestech.es>
2018-11-04 12:30:17 +0100

    Ejericio 21 terminado CORREGIDO

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,100 +20,118 @@ public class Main<Ejercicio5> {
         int numEjercicio = sc1.nextInt();
 
         if (numEjercicio==5) {
-            // Abrimos la variable escaner para la introducción de datos mediante teclado.
-            Scanner sc = new Scanner(System.in);
+            Ejercicio5();
 
-            //Información para el usuario
-            System.out.println("En este ejercicio el usuario introducirá dos número y el programa dirá si son iguales o no");
-
-            //Pedimos al usuario que introduzca el primer número
-            System.out.println("Introduzca el primer número");
-            //Guardamos en la variable el primer valor
-            double num1 = sc.nextDouble();
-            //Pedimos al usuario que introduzca el segundo número
-            System.out.println("Introduzca el segundo número");
-            // Guardamos en la variable num2 el valor introducido
-            double num2 = sc.nextDouble();
-
-            //Se hacen las comparaciones para indicar si es igual o no
-            if (num1==num2) {
-                System.out.println("Los números introducidos son iguales");
-            } else
-                System.out.println("Los números introducidos no son iguales");
         } else if (numEjercicio==6) {
-            // Abrimos la variable escaner para la introducción de datos mediante teclado.
-            Scanner sc = new Scanner(System.in);
-
-            //Información para el usuario
-            System.out.println("En este ejercicio el usuario introducirá dos números enteros y el programa dirá si son multipolos o no");
-
-            //Pedimos al usuario que introduzca el primer número
-            System.out.println("Introduzca el primer número entero");
-            //Guardamos en la variable el primer valor
-            int num1 = sc.nextInt();
-            //Pedimos al usuario que introduzca el segundo número
-            System.out.println("Introduzca el segundo número entero");
-            // Guardamos en la variable num2 el valor introducido
-            int num2 = sc.nextInt();
-
-            //Se hacen las comparaciones para indicar si es multiplo o no
-            if (num1%num2==0) {
-                System.out.printf("El número %d es multiplo de %d",num1,num2);
-            } else
-                System.out.printf("El número %d no es multiplo de %d",num1,num2);
+            Ejercicio6();
         }else if (numEjercicio==7) {
-            // Abrimos la variable escaner para la introducción de datos mediante teclado.
-            Scanner sc = new Scanner(System.in);
-
-            //Información para el usuario
-            System.out.println("En este ejercicio el usuario introducirá dos números y el programa dira cual es mayor o si son iguales");
-
-            //Pedimos al usuario que introduzca el primer número
-            System.out.println("Introduzca el primer número entero");
-            //Guardamos en la variable el primer valor
-            double num1 = sc.nextDouble();
-            //Pedimos al usuario que introduzca el segundo número
-            System.out.println("Introduzca el segundo número entero");
-            // Guardamos en la variable num2 el valor introducido
-            double num2 = sc.nextDouble();
-
-            //Se hacen las comparaciones para indicar si es multiplo o no
-            if (num1>num2) {
-                System.out.println("El numero mayor es: "+num1);
-            } else if (num2>num1) {
-                System.out.println("El número mayor es: "+num2);
-            } else if (num1==num2) {
-                System.out.println("Los numeros son iguales");
-            }
+            Ejercicio7();
         }else if (numEjercicio==8) {
-            // Abrimos la variable escaner para la introducción de datos mediante teclado.
-            Scanner sc = new Scanner(System.in);
-
-            //Información para el usuario
-            System.out.println("En este ejercicio el usuario introducirá dos números y el programa lor ordenará de mayor a menor");
-
-            //Pedimos al usuario que introduzca el primer número
-            System.out.println("Introduzca el primer número");
-            //Guardamos en la variable el primer valor
-            double num1 = sc.nextDouble();
-            //Pedimos al usuario que introduzca el segundo número
-            System.out.println("Introduzca el segundo número");
-            // Guardamos en la variable num2 el valor introducido
-            double num2 = sc.nextDouble();
-
-            //Se hacen las comparaciones para ordenarlos
-            if (num1>num2) {
-                System.out.println(num1 +" " + num2);
-            } else if (num2>num1) {
-                System.out.println(num2 + " "+ num1);
-            } else if (num1==num2) {
-                System.out.println("Los numeros son iguales");
-            }
+            Ejercicio8();
         }
 
 
 
 
+    }
+
+    public static void Ejercicio5 () {
+        // Abrimos la variable escaner para la introducción de datos mediante teclado.
+        Scanner sc = new Scanner(System.in);
+
+        //Información para el usuario
+        System.out.println("En este ejercicio el usuario introducirá dos número y el programa dirá si son iguales o no");
+
+        //Pedimos al usuario que introduzca el primer número
+        System.out.println("Introduzca el primer número");
+        //Guardamos en la variable el primer valor
+        double num1 = sc.nextDouble();
+        //Pedimos al usuario que introduzca el segundo número
+        System.out.println("Introduzca el segundo número");
+        // Guardamos en la variable num2 el valor introducido
+        double num2 = sc.nextDouble();
+
+        //Se hacen las comparaciones para indicar si es igual o no
+        if (num1==num2) {
+            System.out.println("Los números introducidos son iguales");
+        } else
+            System.out.println("Los números introducidos no son iguales");
+
+    }
+
+    public static void Ejercicio6 () {
+        // Abrimos la variable escaner para la introducción de datos mediante teclado.
+        Scanner sc = new Scanner(System.in);
+
+        //Información para el usuario
+        System.out.println("En este ejercicio el usuario introducirá dos números enteros y el programa dirá si son multipolos o no");
+
+        //Pedimos al usuario que introduzca el primer número
+        System.out.println("Introduzca el primer número entero");
+        //Guardamos en la variable el primer valor
+        int num1 = sc.nextInt();
+        //Pedimos al usuario que introduzca el segundo número
+        System.out.println("Introduzca el segundo número entero");
+        // Guardamos en la variable num2 el valor introducido
+        int num2 = sc.nextInt();
+
+        //Se hacen las comparaciones para indicar si es multiplo o no
+        if (num1%num2==0) {
+            System.out.printf("El número %d es multiplo de %d",num1,num2);
+        } else
+            System.out.printf("El número %d no es multiplo de %d",num1,num2);
+
+    }
+
+    public static void Ejercicio7 () {
+        // Abrimos la variable escaner para la introducción de datos mediante teclado.
+        Scanner sc = new Scanner(System.in);
+
+        //Información para el usuario
+        System.out.println("En este ejercicio el usuario introducirá dos números y el programa dira cual es mayor o si son iguales");
+
+        //Pedimos al usuario que introduzca el primer número
+        System.out.println("Introduzca el primer número entero");
+        //Guardamos en la variable el primer valor
+        double num1 = sc.nextDouble();
+        //Pedimos al usuario que introduzca el segundo número
+        System.out.println("Introduzca el segundo número entero");
+        // Guardamos en la variable num2 el valor introducido
+        double num2 = sc.nextDouble();
+
+        //Se hacen las comparaciones para indicar si es multiplo o no
+        if (num1>num2) {
+            System.out.println("El numero mayor es: "+num1);
+        } else if (num2>num1) {
+            System.out.println("El número mayor es: "+num2);
+        } else if (num1==num2) {
+            System.out.println("Los numeros son iguales");
+        }
+    }
+    public static void Ejercicio8 () {
+        // Abrimos la variable escaner para la introducción de datos mediante teclado.
+        Scanner sc = new Scanner(System.in);
+
+        //Información para el usuario
+        System.out.println("En este ejercicio el usuario introducirá dos números y el programa lor ordenará de mayor a menor");
+
+        //Pedimos al usuario que introduzca el primer número
+        System.out.println("Introduzca el primer número");
+        //Guardamos en la variable el primer valor
+        double num1 = sc.nextDouble();
+        //Pedimos al usuario que introduzca el segundo número
+        System.out.println("Introduzca el segundo número");
+        // Guardamos en la variable num2 el valor introducido
+        double num2 = sc.nextDouble();
+
+        //Se hacen las comparaciones para ordenarlos
+        if (num1>num2) {
+            System.out.println(num1 +" " + num2);
+        } else if (num2>num1) {
+            System.out.println(num2 + " "+ num1);
+        } else if (num1==num2) {
+            System.out.println("Los numeros son iguales");
+        }
     }
 
 }
