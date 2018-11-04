d7a7dd7b88b59845bd0bc599cd230a991242ae88
Portatil-Juan <juan.alvarez@escuelaestech.es>
2018-11-04 13:14:59 +0100

    Ejericio 20 EN PROCESO

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,31 +12,93 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         //Información para el usuario
-        System.out.println("En este ejercicio el usuario introducirá un numero entre 0 y 9999 y mostrara las cifras al revés.");
+        System.out.println("En este ejercicio el usuario introducirá un numero entre 0 y 99 y el programa lo mostrara en texto");
 
         //Pedimos al usuario que introduzca el primer número
-        System.out.println("Introduzca un número dentro del rango");
+        System.out.println("Introduzca un número entero dentro del rango 0 y 99");
         //Guardamos en la variable el primer valor
         int num1 = sc.nextInt();
 
-        //Se hacen las comparaciones y operaciones para mostrar el numero de cifras.
-        if (num1>=0&&num1<=9999) {
-            String num = String.valueOf(num1);
-            String reves ="";
 
-            char[] numReves = num.toCharArray();
 
-            for (int i=numReves.length-1; i>-1;i--) {
-                reves+=numReves[i]+"";
-            }
-            System.out.println("El numero introducido con las cifras al reves es: "+reves);
-
-        } else {
-            System.out.println("El número introducido esta fura del rango 0 y 9999");
+    }
+    public static String Unidad (int nUnidad){
+        switch (nUnidad) {
+            case 1:
+                return "uno";
+            case 2:
+                return "dos";
+            case 3:
+                return "tres";
+            case 4:
+                return "cuatro";
+            case 5:
+                return "cinco";
+            case 6:
+                return "seis";
+            case 7:
+                return "siete";
+            case 8:
+                return "ocho";
+            case 9:
+                return "nueve";
+            case 0:
+                return "cero";
+            default:
+                return "";
         }
 
+    }
 
+    public static String decena (int nDecena) {
+        switch (nDecena) {
+            case 1:
+                return "diez";
+            case 2:
+                return "veinte";
+            case 3:
+                return "treinta";
+            case 4:
+                return "cuarenta";
+            case 5:
+                return "cincuenta";
+            case 6:
+                return "sesenta";
+            case 7:
+                return "setenta";
+            case 8:
+                return "ochenta";
+            case 9:
+                return "noventa";
+            default:
+                return "";
+        }
 
+    }
+
+    public static String decenaExacta (int decenas){
+        switch (decenas) {
+            case 11:
+                return "once";
+            case 12:
+                return "doce";
+            case 13:
+                return "trece";
+            case 14:
+                return "catorce";
+            case 15:
+                return "quince";
+            case 16:
+                return "dieciseis";
+            case 17:
+                return "diecisiete";
+            case 18:
+                return "dieciocho";
+            case 19:
+                return "diecinueve";
+            default:
+                return "";
+        }
     }
 
 }
